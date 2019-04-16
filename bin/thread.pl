e5f3abf4b06bcb7c600b5cdaede4edf88cadcb64
xavierlee17 <39209606+xavierlee17@users.noreply.github.com>
2019-04-16 16:28:18 +0800

    Create thread.pl

---
+++ bin/thread.pl
@@ -0,0 +1,17 @@
+#/usr/bin/env perl
+
+use threads ('exit'=>'threads_only');
+use threads::shared;
+
+open(LIST,@ARGV[0]);
+my @list:shared = <LIST>;
+close(LIST);
+while (1) {
+  $running=threads->list(threads::runing);
+  $list=@list;
+  if (($running < @ARGV[1]) && ($list > 0)) {threads->new(\&job);}
+  if ($running > 0) {foreach $tmp(threads->list(threads::all)) {if ($tmp->is_joinable()){$tmp->join();}}}
+  last if ($list == 0);
+}
+foreach $tmp(threads->list(threads::running)) {$tmp->join();}
+sub job {$tmp = shift(@list);$tmp=~s/\n//;if ($tmp) {$thr = threads->tid();system "$tmp $thr\n";exit(1);}}
