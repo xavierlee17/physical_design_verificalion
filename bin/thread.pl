9d5da73ae56ccff14845d283d88b8449fabc091c
xavierlee17 <39209606+xavierlee17@users.noreply.github.com>
2019-04-17 10:39:56 +0800

    Update thread.pl

--- bin/thread.pl
+++ bin/thread.pl
@@ -7,9 +7,10 @@ open(LIST,@ARGV[0]);
 my @list:shared = <LIST>;
 close(LIST);
 while (1) {
-  $running=threads->list(threads::runing);
-  $list=@list;
+  $running=threads->list(threads::runing);$list=@list;
   if (($running < @ARGV[1]) && ($list > 0)) {threads->new(\&job);}
+  $running=threads->list(threads::runing);$list=@list;
+  if ($running >= @ARGV[1]) {sleep(1);} else {if ($list > 0) {next;}}
   if ($running > 0) {foreach $tmp(threads->list(threads::all)) {if ($tmp->is_joinable()){$tmp->join();}}}
   last if ($list == 0);
 }
